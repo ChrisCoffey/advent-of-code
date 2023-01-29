dbc208801c4fe1935c1878606c53dcb95f689e84
Chris Coffey <cmcof273@gmail.com>
2023-01-29 17:39:10 -0500

    bfs for aoc day 11 2016

--- haskell/src/TwentySixteen/Day11.hs
+++ haskell/src/TwentySixteen/Day11.hs
@@ -1,234 +1,133 @@
 module TwentySixteen.Day11 (
-  part1,
-  part2
 ) where
 
 import Protolude
 import qualified Data.Map as M
 import qualified Data.List as L
 import qualified Data.Sequence as S
+import qualified Data.Text as T
 import qualified Data.Set as Set
 
 import Utils
+import GHC.Natural (Natural)
 
--- This solution actually makes all of the moves.
+newtype Generator = Generator Text
+  deriving (Eq, Ord)
 
+newtype Chip = Chip Text
+  deriving (Eq, Ord)
 
-part1 :: IO Int
-part1 = pure 42
+compChipGen :: Chip -> Generator -> Bool
+compChipGen (Chip t) (Generator s) = t == s
 
-part2 :: IO Int
-part2 = pure 42
+data Floor = Floor { chips :: [Text], generators :: [Text] }
+  deriving (Eq, Ord)
 
-data BoardState = BS {bsFloors :: Map Int [Thing], elevator:: Int}
-  deriving (Show, Eq, Ord)
-
-floorFromState ::
-  BoardState
-  -> Int
-  -> [Thing]
-floorFromState s floorNum = bsFloors s M.! floorNum
-
-terminalState ::
-  BoardState
-  -> Bool
-terminalState state =
-  (elevator state == 3) &&
-  null (bsFloors state M.! 0) &&
-  null (bsFloors state M.! 1) &&
-  null (bsFloors state M.! 2)
-
-type StateGraph = Map BoardState [BoardState]
-
-newtype Floor = Floor [Thing]
-  deriving (Show, Eq)
-
--- How far does each element need to move to reach the top floor?
-type Scores = Map Thing Int
-
-data Thing
-  = Generator Text
-  | Chip Text
-  deriving (Show, Eq, Ord)
-
-isChip (Chip _) = True
-isChip _ = False
-
-isGenerator (Generator _) = True
-isGenerator _ = False
-
-matchedPair ::
-  Thing
-  -> Thing
-  -> Bool
-matchedPair (Generator a) (Chip b) = a == b
-matchedPair (Chip a) (Generator b) = a == b
-matchedPair _ _ = False
-
-
--- a BFS search through a move graph
-findShortestPath ::
-  StateGraph
-  -> BoardState
-  -> Int
-findShortestPath graph initialState =
-  go (S.singleton (initialState, 0)) Set.empty
-  where
-    go ((s, dist) S.:<| frontier) seen
-      | terminalState s = dist
-      | otherwise = let
-        edges = graph M.! s
-        frontierStates = fst <$> frontier
-        novelStates = filter (`Set.notMember` seen) edges
-        withDistances = (, dist +1) <$> novelStates
-        f = if elevator s == 3
-            then traceShow (s, dist) go
-            else go
-        in go (frontier S.>< S.fromList withDistances) (Set.insert s seen)
-
--- The current failure suggests that the graph is incomplete? How many nodes should/could it have?
-
-createMoveGraph ::
-  BoardState
-  -> Int
-createMoveGraph initialState =
-  go (S.singleton (initialState, 0)) Set.empty
-  where
-    go S.Empty stateGraph = -1
-    go ((s, dist)  S.:<| frontierStates) seen
-      | terminalState s = dist
-      | otherwise = let
-          focusedFloor = floorFromState s $ elevator s
-          maxFloor = 3
-          minFloor = 0
-
-          (upwardPairs, upwardSingle) =
-            if elevator s < maxFloor
-            then movesFromFloor focusedFloor (floorFromState s (elevator s + 1))
-            else ([], [])
-          upwardStates =
-            if elevator s < maxFloor
-            then updateState 1 s <$> moveList upwardPairs (if L.null upwardPairs then upwardSingle else []) -- Only move pairs up if possible
-            else []
-
-          emptyBelow = all identity [ L.null (floorFromState s n) | n <- [0 .. max (elevator s - 1) 0] ]
-          (downwardPairs, downwardSingle) =
-            if elevator s > minFloor && not emptyBelow
-            then movesFromFloor focusedFloor (floorFromState s (elevator s - 1))
-            else ([], [])
-          downwardStates =
-            if elevator s > minFloor && not emptyBelow
-            then updateState (-1) s <$> moveList (if L.null downwardSingle then downwardPairs else []) downwardSingle -- Only move a single items down if possible
-            else []
-
-          novelStates = (, dist + 1) <$> filter (\k -> not $ k `Set.member` seen) (upwardStates <> downwardStates)
-          seen' = foldl (flip Set.insert) seen (fst <$> novelStates)
-
-          updatedFrontier = frontierStates S.>< S.fromList novelStates
-          in go updatedFrontier seen'
-
-addStateToGraph ::
-  BoardState -- Origin
-  -> StateGraph -- Graph
-  -> BoardState -- Destination
-  -> StateGraph
-addStateToGraph origin graph destination = let
-  graph' = M.insertWith (\new existing -> existing <> new) destination [origin] graph -- Using an empty list rather than the origin keeps the graph directed. Should this be a directed graph?
-  in M.insertWith (\new existing -> existing <> new) origin [destination] graph'
-
-
-updateState ::
-  Int -- Elevator change
-  -> BoardState -- starting state
-  -> [Thing] -- move
-  -> BoardState
-updateState elevatorChange startingState move =
-  BS {
-    elevator = targetNum
-  , bsFloors = updatedFloors
+data Building = Building {
+    groundF :: Floor
+  , secondF :: Floor
+  , thirdF :: Floor
+  , fourthF :: Floor
   }
+  deriving (Eq, Ord)
+
+
+-- Transform a Building into an somewhat ugly but legible text representation
+prettyPrint :: Building -> Text
+prettyPrint building =
+  T.intercalate ("\n" :: Text) [
+    prettyFloor (fourthF building)
+  , prettyFloor (thirdF building)
+  , prettyFloor (secondF building)
+  , prettyFloor (groundF building)
+  ]
   where
-    floorNum = elevator startingState
-    targetNum =  floorNum + elevatorChange
-    startingFloor = (bsFloors startingState) M.! floorNum
-    targetFloor = (bsFloors startingState) M.! targetNum
-    (fromFloor, toFloor) = doMove move startingFloor targetFloor
+    prettyFloor :: Floor -> Text
+    prettyFloor (Floor chips generators) = T.intercalate ("  " :: Text) $ map prettyGen (sort generators) <> map prettyChip (sort chips)
 
-    updatedFloors = M.insert targetNum toFloor $
-      M.insert floorNum fromFloor (bsFloors startingState)
-
-doMove ::
-  [Thing]
-  -> [Thing]
-  -> [Thing]
-  -> ([Thing], [Thing])
-doMove t fromFloor toFloor =
-  (filter (\a -> not $ a `elem` t) fromFloor, t<>toFloor)
-
--- Determine which moves can be made between two floors, making sure both floors are in a valid state afterwards
-movesFromFloor ::
-  [Thing]
-  -> [Thing]
-  -> ([(Thing, Thing)], [Thing])
-movesFromFloor fromFloor toFloor =
-  (pairedMoves, singleMoves)
-  where
-    singleMoves = filter (\thing -> checkFloors (doMove [thing] fromFloor toFloor)) fromFloor
-    pairedMoves = filter (\(a,b) -> checkFloors (doMove [a,b] fromFloor toFloor)) thingPairs
-
-    checkFloors (from, to) = floorIsSafe from && floorIsSafe to
-
-    thingPairs = pairs fromFloor
-
-floorIsSafe ::
-  [Thing]
-  -> Bool
-floorIsSafe things =
-    all (\t -> any (matchedPair t) things) (filter isChip things) || -- All chips are matched to a generator
-    all isChip things || -- The floor is only chips
-    all isGenerator things -- The floor is only generators
+    prettyGen t = "G " <> t
+    prettyChip t = "C " <> t
 
 
-moveList tupled single = ((\(a,b) -> [a, b]) <$> tupled) <> ((: []) <$> single)
+-- A move is a (Building, Elevator) pair
 
-problem1Input =
-  BS {
-    elevator = 0
-  , bsFloors = M.fromList [
-    (0, [
-          Chip "a"
-        , Generator "a"
-        , Generator "b"
-        , Generator "c"
-      ]
-    ),
-    (1, [
-        Chip "c"
-      , Chip "b"
-      ]
-    ),
-    (2, [
-        Generator "d"
-      , Chip "d"
-      , Generator "e"
-      , Chip "e"
-    ]),
-    (3, [])
-    ]
+-- From the given move, push each valid move onto a queue
+-- Then, pop each move off the queue and evaluate it
+-- If the move is invalid, continue on
+-- If the move is valid, check if the move is a solution
+-- Return to the first step and continue
+
+data ElevatorPosition = FirstF | SecondF | ThirdF | FourthF deriving (Eq, Ord)
+newtype Node = Node (Building, ElevatorPosition)
+  deriving (Eq, Ord)
+
+data Move = Move {
+    node :: Node
+  , depth :: Natural
   }
 
-testInput =
-  BS {
-    elevator = 0,
-    bsFloors = M.fromList [
-      (0, [Chip "h", Chip "l"])
-    , (1, [Generator "h"])
-    , (2, [Generator "l"])
-    , (3, [])
-    ]
-  }
 
-terminalInputState =
-  BS {elevator = 3,
-    bsFloors = M.fromList [(0, []), (1, []), (2, []), (3, concatMap (\c -> [Chip c, Generator c]) ["a", "b", "c", "d", "e"] )]
-    }
+
+breadthFirstSearch ::
+  Building ->
+  Set.Set Node ->
+  [Move] ->
+  Natural
+breadthFirstSearch _ _ [] = 0
+breadthFirstSearch finalState seenPositions (move:moves)
+  | Set.member (node move) seenPositions = breadthFirstSearch finalState seenPositions moves
+  | invalid move = breadthFirstSearch finalState updatedPositions moves
+  | solution (node move) = depth move
+  | otherwise = let
+    d = depth move
+    nextMoves = \n -> Move n (d+1) <$> (validNodes $ node move)
+    in breadthFirstSearch finalState updatedPositions nextMoves
+
+  where
+    -- Generate all one or two item permutations from the current floor
+    validNodes (Node (building, FirstF)) = map (atFloor SecondF) $ allMoveSets (secondF building) (groundF building)
+    validNodes (Node (building, SecondF)) = let
+      movesDown = atFloor FirstF <$> allMoveSets (groundF building) (secondF building)
+      movesUp = atFloor ThirdF <$> allMoveSets (thirdF building) (secondF building)
+      in movesDown <> movesUp
+    validNodes (Node (building, ThirdF)) = let
+      movesDown = atFloor SecondF <$> allMoveSets (secondF building) (thirdF building)
+      movesUp = atFloor FourthF <$> allMoveSets (fourthF building) (thirdF building)
+      in movesDown <> movesUp
+    validNodes (Node (building, FourthF)) = map (\b -> Node (b, ThirdF)) $ allMoveSets (thirdF building) (fourthF building)
+
+    allMoveSets (Floor destChips destGens) (Floor chips gens) = let
+      oneChip = (\c -> Floor (c <> destChips) destGens) <$> chips `choose` 1
+      twoeChips = (\cs -> Floor (cs <> destChips) destGens) <$>  chips `choose` 2
+      oneGen = (\g -> Floor destChips (g <> destGens)) <$> gens`choose` 1
+      twoGens = (\gs -> Floor destChips (gs <> destGens)) <$> gens`choose` 2
+      mixed = (\(c,g) -> Floor (c: destChips) (g:destGens)) <$> pairwisePerms chips gens
+      in oneChip <> twoeChips <> oneGen <> twoGens <> mixed
+
+    -- TODO update the floor in the building
+    atFloor f b = Node (b, f)
+
+    updatedPositions = Set.insert (node move) seenPositions
+
+    invalid (Move (Node (building, elevatorLoc)) _) = noItemsOnFloor elevatorLoc || friedChip
+      where
+        noItemsOnFloor FirstF = emptyFloor $ groundF building
+        noItemsOnFloor SecondF = emptyFloor $ secondF building
+        noItemsOnFloor ThirdF = emptyFloor $ thirdF building
+        noItemsOnFloor FourthF = emptyFloor $ fourthF building
+
+        friedChip = not $ all safeFloor [groundF building, secondF building, thirdF building, fourthF building]
+
+        safeFloor (Floor chips generators) = all (`elem` generators) chips || null generators
+        emptyFloor (Floor chips generaors) = null chips && null generaors
+
+    -- make sure these are sorted
+    solution (Node (building, FourthF)) = prettyPrint building == prettyPrint finalState
+    solution _ = False
+
+testInput = Node (Building {
+    groundF = Floor ["H", "L"] [] ,
+    secondF = Floor [] ["H"],
+    thirdF = Floor [] ["G"],
+    fourthF = Floor [] []
+    }, FirstF)
--- haskell/src/Utils.hs
+++ haskell/src/Utils.hs
@@ -9,3 +9,13 @@ pairs [] = []
 
 notEmpty :: [a] -> Bool
 notEmpty = not . null
+
+choose :: [a] -> Int -> [[a]]
+choose _ 0 = [[]]
+choose [] _ = []
+choose (x:xs) n = (map (\ys -> x:ys) (choose xs (n-1))) ++ (choose xs n)
+
+pairwisePerms :: [a] -> [b] -> [(a, b)]
+pairwisePerms [] _ = []
+pairwisePerms _ [] = []
+pairwisePerms (x: xs) (y: ys) = (x, y) : ((pairwisePerms (x:xs) ys) <> pairwisePerms xs (y: ys))
