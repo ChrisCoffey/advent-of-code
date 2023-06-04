eacb9c8c01c87c3051a8a524a7bf206ef9a428d2
Chris Coffey <cmcof273@gmail.com>
2023-06-03 21:59:21 -0400

    Manually calculated the result

--- haskell/src/TwentySixteen/Day22.hs
+++ haskell/src/TwentySixteen/Day22.hs
@@ -4,6 +4,7 @@ import Protolude
 import qualified Data.Text as Text
 import qualified Data.Sequence as Seq
 import Data.List ((!!))
+import qualified Data.Text as T
 
 type Coords = (Int, Int)
 data Node = Node {
@@ -13,6 +14,9 @@ data Node = Node {
   , avail :: Int
   }
   deriving (Eq, Ord, Show)
+
+data NodeType = Viable | Empty | Stuck
+  deriving (Eq, Show)
 
 toNode ::
   Text
@@ -58,16 +62,107 @@ viablePairs ::
 viablePairs nodeA nodes =
   foldl' checkViable [] nodes
   where
-    checkViable acc nodeB =
-      go nodeA nodeB $ go nodeB nodeA acc
-      where
-        go a b acc
-          -- Guards
-          | coords a == coords b = acc
-          | used a <= 0 = acc
-          -- valid
-          | used a <= avail b = (a, b):acc
-          | otherwise = acc
+  checkViable acc nodeB =
+    go nodeA nodeB $ go nodeB nodeA acc
+    where
+      go a b acc
+        -- Guards
+        | coords a == coords b = acc
+        | used a <= 0 = acc
+        -- valid
+        | used a <= avail b = (a, b):acc
+        | otherwise = acc
+
+findViable ::
+  (Node -> Node -> Bool)
+  -> Node
+  -> [Node]
+  -> [Node]
+findViable pred node otherNodes =
+  filter (check node ) otherNodes
+  where
+    check a b
+      | coords a == coords b = False
+      | used a <= 0 = False
+      | otherwise  = pred a b
+
+
+toNodeTypes ::
+  [Node]
+  -> [(Coords, NodeType)]
+toNodeTypes nodes =
+  (\n -> (coords n, nodeMapping n)) <$> nodes
+  where
+    nodeMapping node
+      | used node == 0 = Empty
+      | null (viablePairs node nodes) = Stuck
+      | otherwise = Viable
+
+
+
+
+drawGrid ::
+  [Node]
+  -> Text
+drawGrid =
+  T.intercalate "\n" . fmap drawRow . asRows
+  where
+  drawRow = T.intercalate "   " . fmap drawCell
+
+drawCell ::
+  Node
+  -> Text
+drawCell node = (show (used node)) <> "/" <> (show (size node ))
+
+-- Coordinate sorting is working correctly
+checkCoords ::
+  Node
+  -> Text
+checkCoords node = show ((fst . coords) node ) <> "," <> (show (snd $ coords node))
+
+
+drawTypeGrid ::
+  [Node]
+  -> Text
+drawTypeGrid nodes = T.intercalate "\n" . fmap drawRow . asRowsT $ toNodeTypes nodes
+  where
+    drawRow = T.intercalate "" . fmap drawType
+    asRowsT = groupBy (\l r -> col l == col r) . sortOn col
+    col = snd . fst
+
+drawType ::
+  (Coords, NodeType)
+  -> Text
+drawType (_, Empty) = "_"
+drawType (_, Stuck) = "#"
+drawType (_, Viable) = "."
+
+
+
+
+asRows ::
+  [Node]
+  -> [[Node]]
+asRows =
+  groupBy (\l r -> col l == col r) . sortOn col
+  where
+    col = snd . coords
+
+
+
+part2 ::
+  FilePath
+  -> IO Int
+part2 path = do
+  raw <- readFile path
+  let nodes = allNodes raw
+      grid = drawTypeGrid nodes
+  putStrLn grid
+  pure 42
+
+
+
+
 
 
 
@@ -84,6 +179,3 @@ part1 path = do
   where
     calcPairs [] = []
     calcPairs (a:rest) = (viablePairs a rest) <> calcPairs rest
-
-
-
